1328952d28862e0cbf034b676d9f76e2b6a2bb2a
Caio <caiocaracas1@gmail.com>
2025-09-15 21:46:28 -0300

    matmul_vec function (check row-major)

--- sw/c_infer/src/infer.c
+++ sw/c_infer/src/infer.c
@@ -12,4 +12,11 @@ static void normalize_inplace(float *x, int d) {
         x[i] = (x[i] - MLP_FEAT_MEAN[i]) / (MLP_FEAT_SCALE[i] + 1e-12f);
     }
 }
-
+static void matmul_vec(const float *x, const float *W, int d, int m, float *y) {
+  for (int j = 0; j < m; ++j) {
+    float acc = 0.f;
+    const float *wcol = &W[j];    
+    for (int i = 0; i < d; ++i) acc += x[i] * wcol[i * m];
+    y[j] = acc;
+  }
+}
