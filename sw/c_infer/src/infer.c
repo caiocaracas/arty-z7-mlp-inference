b766750d93cca845e1e0d9ac9da9662df8799f6c
Caio <caiocaracas1@gmail.com>
2025-09-15 21:49:55 -0300

    mlp_foward_logits function

--- sw/c_infer/src/infer.c
+++ sw/c_infer/src/infer.c
@@ -25,3 +25,19 @@ static int argmax(const float *v, int n) {
   for (int i = 1; i < n; ++i) if (v[i] > best) { best = v[i]; idx = i; }
   return idx;
 }
+static void mlp_forward_logits(const float *x_raw, float *z2_out) {
+  static float x[4096];   // buffers simples; cabe folgado para este MLP
+  static float z1[2048];
+  static float h[2048];
+
+  for (int i = 0; i < MLP_N_IN; ++i) x[i] = x_raw[i];
+  normalize_inplace(x, MLP_N_IN);
+
+  // z1 = x*W1 + b1
+  matmul_vec(x, MLP_W1, MLP_N_IN, MLP_N_HID, z1);
+  for (int j = 0; j < MLP_N_HID; ++j) h[j] = relu(z1[j] + MLP_B1[j]);
+
+  // z2 = h*W2 + b2
+  matmul_vec(h, MLP_W2, MLP_N_HID, MLP_N_OUT, z2_out);
+  for (int k = 0; k < MLP_N_OUT; ++k) z2_out[k] += MLP_B2[k];
+}
