ec78a76720d9aadb4571ef64337a110b5efb1abd
Caio <caiocaracas1@gmail.com>
2025-09-15 21:45:14 -0300

    normalize_inplace function

--- sw/c_infer/src/infer.c
+++ sw/c_infer/src/infer.c
@@ -5,3 +5,11 @@
 #include <math.h>
 #include "../export/weights.h" 
 
+static inline float relu(float x) {return x > 0.f ? x : 0.f; }
+
+static void normalize_inplace(float *x, int d) {
+    for (int i = 0; i < d; ++i) {
+        x[i] = (x[i] - MLP_FEAT_MEAN[i]) / (MLP_FEAT_SCALE[i] + 1e-12f);
+    }
+}
+
